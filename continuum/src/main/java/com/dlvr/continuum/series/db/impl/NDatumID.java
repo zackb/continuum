4157d6df43795de0da67c652329bf2b952bbc155
Zack Bartel <zack@bartel.com>
2016-02-11 12:00:27 -0800

    byte-fu

--- continuum/src/main/java/com/dlvr/continuum/series/db/impl/NDatumID.java
+++ continuum/src/main/java/com/dlvr/continuum/series/db/impl/NDatumID.java
@@ -1,6 +1,8 @@
 package com.dlvr.continuum.series.db.impl;
 
 import com.dlvr.continuum.series.datum.Datum;
+import com.dlvr.continuum.series.datum.Tags;
+import com.dlvr.continuum.series.db.DatumID;
 import com.dlvr.continuum.series.db.ID;
 import com.dlvr.continuum.util.Bytes;
 
@@ -9,13 +11,22 @@ import java.nio.ByteBuffer;
 /**
  * Created by zack on 2/11/16.
  */
-public class NDatumID implements ID {
+public class NDatumID implements DatumID {
 
     private static final byte b = 0x00;
 
     private transient byte[] cachedId;
+    private final int[] positions;
 
     public NDatumID(byte[] bytes) {
+        int count = 0;
+        for (byte by : bytes)
+            if (by == b) count++;
+        positions = new int[count];
+        int posi = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == b) positions[posi++] = i;
+        }
         this.cachedId = bytes;
     }
 
@@ -24,6 +35,9 @@ public class NDatumID implements ID {
         byte[] tags = datum.tags().ID().bytes();
         byte[] ts = Bytes.bytes(datum.timestamp());
         byte[] id = new byte[name.length + tags.length + ts.length + 2];
+
+        positions = new int[] {name.length + 1, tags.length + 1, ts.length + 2};
+
         ByteBuffer buff = ByteBuffer.wrap(id);
         buff.put(name);
         buff.put(b);
@@ -41,5 +55,20 @@ public class NDatumID implements ID {
     @Override
     public byte[] bytes() {
         return cachedId;
+    }
+
+    @Override
+    public String name() {
+        return Bytes.String(Bytes.range(cachedId, 0, positions[0]));
+    }
+
+    @Override
+    public Tags tags() {
+        return null;
+    }
+
+    @Override
+    public long timestamp() {
+        return 0;
     }
 }
---
+++ continuum/src/test/java/com/dlvr/continuum/series/db/DBTest.java
@@ -0,0 +1,42 @@
+package com.dlvr.continuum.series.db;
+
+import com.dlvr.continuum.io.file.impl.FileSystemReference;
+import com.dlvr.continuum.series.datum.Datum;
+import com.dlvr.continuum.series.db.impl.RockDB;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.dlvr.continuum.Continuum.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by zack on 2/11/16.
+ */
+public class DBTest {
+    FileSystemReference reference = new FileSystemReference("/Users/zack/Desktop/DBBB");
+    @Test
+    public void testScan() throws Exception {
+        Map<String, String> tags = new HashMap<>();
+        tags.put("tag1", "tagvalue1");
+        tags.put("tag2", "2tagvalue2");
+        tags.put("tag3", "atagvalue3");
+
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("fields", "ffffffff");
+        fields.put("fields1", "vfield");
+        fields.put("fields4", "fieldv");
+
+        DB db = new RockDB("fooe", reference);
+        Datum d = datum("zack")
+                .timestamp(System.currentTimeMillis())
+                .tags(tags(tags))
+                .fields(fields(fields))
+                .value(1234)
+                .build();
+        db.write(d);
+
+        db.query(null);
+    }
+}
