d7ecbcbe3da3fb7f7cf91c42e70ce6877815888c
Zack Bartel <zack@bartel.com>
2016-02-12 15:30:16 -0800

    stub out interface

--- continuum-core/src/main/java/com/dlvr/continuum/Continuum.java
+++ continuum-core/src/main/java/com/dlvr/continuum/Continuum.java
@@ -1,11 +1,14 @@
 package com.dlvr.continuum;
 
+import com.dlvr.continuum.io.file.impl.FileSystemReference;
 import com.dlvr.continuum.series.datum.Datum;
 import com.dlvr.continuum.series.datum.Fields;
 import com.dlvr.continuum.series.datum.Tags;
 import com.dlvr.continuum.series.datum.impl.NDatum;
 import com.dlvr.continuum.series.datum.impl.NFields;
 import com.dlvr.continuum.series.datum.impl.NTags;
+import com.dlvr.continuum.series.db.DB;
+import com.dlvr.continuum.series.db.impl.RockDB;
 import com.dlvr.continuum.series.query.Function;
 import com.dlvr.continuum.series.query.Query;
 import com.dlvr.continuum.series.query.QueryResult;
@@ -20,6 +23,14 @@ import java.util.concurrent.TimeUnit;
  * Created by zack on 2/10/16.
  */
 public class Continuum {
+
+    private final String id;
+
+    private final StorageType type;
+
+    private final DB db;
+
+    private final FileSystemReference base;
 
     public static void sayHello() {
         System.out.printf("People of Earth, how are you?\n");
@@ -43,6 +54,66 @@ public class Continuum {
 
     public static QueryResultBuilder result(String name) {
         return new QueryResultBuilder(name);
+    }
+
+    private Continuum(String id, StorageType type, FileSystemReference base) throws Exception {
+        checkNotNull("id", id);
+        checkNotNull("type", type);
+        checkNotNull("base", base);
+        this.id = id;
+        this.type = type;
+        this.base = base;
+        this.db = new RockDB(id, base);
+    }
+
+    /**
+     * Close all underlying resources for this continuum
+     * @throws Exception on failure
+     */
+    public void close() throws Exception {
+        db.close();
+    }
+
+    /**
+     * CAUTION: Deletes all fileystem resources associated with this continuum
+     * @throws Exception on failure
+     */
+    public void delete() throws Exception {
+        db.close();
+        base.delete();
+    }
+
+    public static Builder continuum() {
+        return new Builder();
+    }
+
+    public static Builder series() {
+        return new Builder().type(StorageType.SERIES);
+    }
+
+    public static Builder kv() {
+        return new Builder().type(StorageType.KEYVALUE);
+    }
+
+    public static class Builder {
+        private StorageType type = StorageType.SERIES;
+        private FileSystemReference base;
+        private String id = "default";
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+        public Builder base(FileSystemReference base) {
+            this.base = base;
+            return this;
+        }
+        public Builder type(StorageType type) {
+            this.type = type;
+            return this;
+        }
+        public Continuum open() throws Exception {
+            return new Continuum(id, type, base);
+        }
     }
 
     public static class DatumBuilder {
@@ -123,6 +194,16 @@ public class Continuum {
 
         public QueryResult build() {
             return target;
+        }
+    }
+
+    private enum StorageType {
+        SERIES,KEYVALUE
+    }
+
+    private void checkNotNull(String name, Object value) throws NullPointerException {
+        if (value == null) {
+            throw new NullPointerException("Option: '" + name + "'must be set");
         }
     }
 }
--- continuum-core/src/test/java/com/dlvr/continuum/ContinuumTest.java
+++ continuum-core/src/test/java/com/dlvr/continuum/ContinuumTest.java
@@ -1,13 +1,26 @@
 package com.dlvr.continuum;
 
+import com.dlvr.continuum.io.file.impl.FileSystemReference;
 import org.junit.Test;
 
 /**
  * Created by zack on 2/10/16.
  */
 public class ContinuumTest {
+
+    FileSystemReference reference = new FileSystemReference("/tmp/continuum/test.com.dlvr.continuum.ContinuumTest");
+
     @Test
     public void testHello() {
         Continuum.sayHello();
+    }
+
+    @Test
+    public void testSanity() throws Exception {
+        Continuum continuum = Continuum.continuum()
+                                .id("testSanity")
+                                .base(reference)
+                                .open();
+        continuum.delete();
     }
 }
