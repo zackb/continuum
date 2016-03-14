f77cce7bfbf6774debb604f5300b35bd0047402f
Zack Bartel <zack@bartel.com>
2016-03-14 14:51:18 -0700

    move load test to cli app

---
+++ continuum/src/main/java/com/dlvr/continuum/Main.java
@@ -0,0 +1,10 @@
+package com.dlvr.continuum;
+
+/**
+ * Created by zack on 3/14/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Continuum.sayHello();
+    }
+}
---
+++ continuum/src/main/java/com/dlvr/continuum/main/LoadTest.java
@@ -0,0 +1,94 @@
+package com.dlvr.continuum.main;
+
+import com.dlvr.continuum.Continuum;
+import com.dlvr.continuum.atom.Atom;
+import com.dlvr.continuum.core.io.file.FileSystemReference;
+import com.dlvr.continuum.db.slice.Function;
+import com.dlvr.continuum.util.Maths;
+import com.dlvr.continuum.util.datetime.Interval;
+import com.dlvr.util.Metrics;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Load test
+ */
+public class LoadTest {
+
+    private Continuum continuum = null;
+
+    LoadTest(Continuum continuum) {
+        this.continuum = continuum;
+    }
+
+    private final TimerTask timer = new MetricTimer().schedule(() -> System.out.println(Metrics.report()), 5000);
+
+    private final TimerTask reader = new MetricTimer().schedule(() -> {
+        try {
+            Metrics.time("read", () -> {
+                    continuum.db().slice(
+                            Continuum.scan("series" + Maths.randInt(0, 100))
+                                    .function(Function.AVG)
+                                    .interval(Interval.valueOf("1m"))
+                                    .build()
+                    );
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } , 500);
+
+    private static Atom createAtom() {
+        return Continuum.satom()
+                .name("series" + Maths.randInt(0, 100))
+                .value(Maths.randDouble(Double.MIN_VALUE, Double.MAX_VALUE))
+                .timestamp(System.currentTimeMillis())
+                .build();
+    }
+
+    private void stop() {
+        timer.cancel();
+        reader.cancel();
+    }
+
+    private void load(final Continuum continuum) {
+        int iterations = 2000000000; //Integer.MAX_VALUE;
+        for (int i = 0; i < iterations; i++) {
+            Metrics.time("write", () -> {
+                continuum.db().write(createAtom());
+                return null;
+            });
+        }
+    }
+
+    static class MetricTimer {
+        private final Timer t = new Timer();
+
+        public TimerTask schedule(final Runnable r, long delay) {
+            final TimerTask task = new TimerTask() { public void run() { r.run(); }};
+            t.scheduleAtFixedRate(task, delay, delay);
+            return task;
+        }
+    }
+
+    // 50MB/10M metrics
+    public static void load(String dataDir, boolean delete) throws Exception {
+        FileSystemReference ref = new FileSystemReference(dataDir);
+
+        Continuum.Builder builder =
+                Continuum.continuum()
+                    .name("com.dlvr.continuum.main.LoadTest")
+                        .base(ref);
+
+        LoadTest test = null;
+        try (Continuum c = builder.open()){
+            test = new LoadTest(c);
+            test.load(c);
+        } finally {
+            if (test != null) test.stop();
+            if (delete) ref.delete();
+        }
+    }
+}
