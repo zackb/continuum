7ac7e5475a08c9f90882a3a074b174d20b5493dd
Zack Bartel <zack@bartel.com>
2016-03-23 13:18:35 -0700

    equals and hashCode

--- continuum-rest/src/main/java/continuum/rest/client/CSlice.java
+++ continuum-rest/src/main/java/continuum/rest/client/CSlice.java
@@ -119,4 +119,36 @@ public class CSlice implements Slice {
     public Stream<Slice> stream() {
         return Stream.of(this);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CSlice)) return false;
+
+        CSlice that = (CSlice) o;
+
+        if (timestamp != that.timestamp) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (values != null ? !values.equals(that.values) : that.values != null) return false;
+        if (children != null ? !children.equals(that.children) : that.children != null) return false;
+        if (atoms != null ? !atoms.equals(that.atoms) : that.atoms != null) return false;
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (values != null ? values.hashCode() : 0);
+        result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
+        result = 31 * result + (children != null ? children.hashCode() : 0);
+        result = 31 * result + (atoms != null ? atoms.hashCode() : 0);
+        return result;
+    }
 }
